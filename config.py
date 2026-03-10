89b2b2809eb14af9be55ae345f7d75e5848515a3
ezhel <ezhel@users.noreply.github.com>
2026-03-10 08:31:16 +0300

    chore: track config.py and remove it from .gitignore

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Discord Bot Settings
+# Get your token from https://discord.com/developers/applications
+# Make sure to enable the 'Message Content Intent'!
+BOT_TOKEN = "BOT_TOKEN_HERE"
+PREFIX = "."
+
+# Pre-defined commands
+COMMANDS = {
+    "status": "systeminfo",
+    "disk": "wmic logicaldisk get caption,freeheight,size",
+    "memory": "systeminfo | findstr /C:\"Total Physical Memory\"",
+    "uptime": "net statistics server",
+    "users": "query user",
+    "processes": "tasklist | more"
+}
